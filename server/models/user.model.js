7e79637a17e2f3fb01eecd697caf54578887d7ae
thiagoeu <araujo.thiago1051@gmail.com>
2025-03-29 18:03:30 -0300

    feat: user model

---
+++ server/models/user.model.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    phone: {
+      type: Number,
+      default: null,
+    },
+    refresh_token: {
+      type: String,
+      default: "",
+    },
+    verify_email: {
+      type: Boolean,
+      default: false,
+    },
+    last_login_date: {
+      type: Date,
+      default: "",
+    },
+    status: {
+      type: String,
+      enum: ["Active", "Inactive", "Banned"],
+      default: "Active",
+    },
+    address_details: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "address",
+      },
+    ],
+    shopping_cart: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "cartProduct",
+      },
+    ],
+    order_history: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "order",
+      },
+    ],
+
+    forgot_password_otp: {
+      type: mongoose.Schema.ObjectId,
+      default: null,
+    },
+    forgot_password_otp_expiry: {
+      type: Date,
+      default: "",
+    },
+    role: {
+      type: Boolean,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+export default UserModel;
