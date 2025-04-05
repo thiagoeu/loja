26e8f68551ac6b0a688f8823c11f54a6a4c1d6f4
thiagoeu <araujo.thiago1051@gmail.com>
2025-04-04 22:34:18 -0300

    start: frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center text-3xl font-bold text-blue-500">
+        Frontend2
+      </div>
+    </>
+  );
+}
+
+export default App;
