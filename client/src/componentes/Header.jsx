fcf4e26ed31e8f9a3ef98cbf363d4687b2d30cbe
thiagoeu <araujo.thiago1051@gmail.com>
2025-04-25 10:52:10 -0300

    refact: mobile version

--- client/src/componentes/Header.jsx
+++ client/src/componentes/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../assets/logo.png";
 import Search from "./Search";
 import { Link } from "react-router-dom";
+import { FaRegCircleUser } from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -25,10 +26,15 @@ const Header = () => {
             />
           </Link>
         </div>
-        <div>
+        <div className="hidden lg:block">
           <Search />
         </div>
-        <div>Carrinho</div>
+        <div className="">
+          <button className="text-neutral-600 lg:hidden">
+            <FaRegCircleUser size={30} />
+          </button>
+          <div className="hidden lg:block">Carrinho</div>
+        </div>
       </div>
     </header>
   );
